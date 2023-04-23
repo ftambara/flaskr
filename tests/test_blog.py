6b8b630a67b6f3e35ced818cd9e71352d44625df
ftambara <98750668+ftambara@users.noreply.github.com>
2023-04-23 17:05:59 -0300

    Add authentication logic checks

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -1,5 +1,45 @@
+import pytest
+
+
+@pytest.fixture
+def user():
+    return {"username": "username", "password": "password"}
+
+
+@pytest.fixture
+def registered_user(client, init_db, user):
+    client.post(
+        "/auth/register",
+        data=user,
+        follow_redirects=True,
+    )
+    return user
+
+
+@pytest.fixture
+def logged_in_user(client, registered_user):
+    client.post(
+        "/auth/login",
+        data=registered_user,
+        follow_redirects=True,
+    )
+    return registered_user
+
+
 class TestBlog:
     def test_blog_index(self, client):
         response = client.get("/blog/")
+        assert response.status_code == 200
+        assert b"Latest Posts" in response.data
+
+    def test_a_logged_out_user_can_access_the_blog_index_page(self, client, init_db):
+        response = client.get("/blog/", follow_redirects=True)
+        assert response.status_code == 200
+        assert b"Latest Posts" in response.data
+
+    def test_a_logged_in_user_can_access_the_blog_index_page(
+        self, client, init_db, logged_in_user
+    ):
+        response = client.get("/blog/", follow_redirects=True)
         assert response.status_code == 200
         assert b"Latest Posts" in response.data
